e952a05fbbe1789d7986357a5b905b075d552101
MarianyAr <mary95_7@hotmail.com>
2017-12-05 21:56:14 -0500

    Se creo la primera vista.

--- src/src/urls.py
+++ src/src/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from apps.dashboard.views import DashboardView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', DashboardView.as_view()),
+    url(r'^admin/', admin.site.urls)
 ]
